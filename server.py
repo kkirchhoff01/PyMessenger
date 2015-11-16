03d427fbd37d1d669c3b6877b8416ff6afb85457
DarrenLade <kkirchhoff01@yahoo.com>
2015-11-16 08:27:10 -0600

    Create server.py

---
+++ server.py
@@ -0,0 +1,76 @@
+import sys
+import socket
+import select
+import time
+
+HOST = 'localhost' 
+SOCKET_LIST = []
+RECV_BUFFER = 4096 
+PORT = 9999
+
+def chat_server():
+
+	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	server_socket.bind((HOST, PORT))
+	server_socket.listen(10)
+
+	# add server socket object to the list of readable connections
+	SOCKET_LIST.append(server_socket)
+ 
+	print "Chat server started on port " + str(PORT)
+ 
+	while 1:
+		#sleep for .1ms to reduce CPU usage (ugly, but it works)
+		time.sleep(0.0001)
+		
+		ready_to_read,ready_to_write,in_error = select.select(SOCKET_LIST,[],[],0)
+	  
+		for sock in ready_to_read:
+            # a new connection request recieved
+			if sock == server_socket: 
+				sockfd, addr = server_socket.accept()
+				SOCKET_LIST.append(sockfd)
+				print "Client (%s, %s) connected" % addr
+
+				broadcast(server_socket, sockfd, "[%s:%s] entered the chat\n" % addr)
+            # a message from a client, not a new connection
+			else:
+				# process data recieved from client, 
+				try:
+					# receiving data from the socket.
+					data = sock.recv(RECV_BUFFER)
+					if data:
+						# there is something in the socket #+ '[' + str(sock.getpeername()) + '] ' +
+						broadcast(server_socket, sock, "\r" + data)  
+					else:
+						# remove the socket that's broken    
+						if sock in SOCKET_LIST:
+							SOCKET_LIST.remove(sock)
+
+						# at this stage, no data means probably the connection has been broken
+						broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr) 
+
+				# exception 
+				except:
+					broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
+					continue
+
+	server_socket.close()
+    
+# broadcast chat messages to all connected clients
+def broadcast (server_socket, sock, message):
+	for socket in SOCKET_LIST:
+		# send the message only to peer
+		if socket != server_socket and socket != sock :
+			try :
+				socket.send(message)
+			except :
+				# broken socket connection
+				socket.close()
+				# broken socket, remove it
+				if socket in SOCKET_LIST:
+					SOCKET_LIST.remove(socket)
+ 
+if __name__ == "__main__":
+	sys.exit(chat_server())
