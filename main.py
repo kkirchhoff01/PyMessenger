675cc2a9e3dc472c18210d7ef214030c6eeca574
Kevin Kirchhoff <kkirchhoff01@yahoo.com>
2016-02-12 12:04:26 -0600

    added comments

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python
 from client import Client
 from server import Server
 import threading
 import sys
 
+# Threading class to run server/client
 class Worker(threading.Thread):
     def __init__(self, server_holder):
         threading.Thread.__init__(self)
@@ -13,19 +15,27 @@ class Worker(threading.Thread):
 
 if __name__ == "__main__":
     fh = open('server_output.log', 'w')
+    # Set output to file to prevent writing to screen
     sys.stdout = fh
+
+    # Init server
     server = Server()
+    # Thread server
     worker = Worker(server)
     worker.start()
+
+    # Init client
     client = Client()
 
     try:
-        client.chat_client()
+        client.chat_client()  # Run main client process
     except(KeyboardInterrupt, SystemExit):
+        # Stop server/client on normal exit
         server.server_stop = True
         fh.close()
         sys.exit(client.end_session())
     except:
+        # Stop server/client on unexpected exit
         server.server_stop = True
         fh.close()
         client.end_session()
